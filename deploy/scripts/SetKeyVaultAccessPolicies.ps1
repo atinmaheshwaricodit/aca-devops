4e5889b5e065fdfbc83bdcd81ee90bf6e981ff58
gitName <a@b.c>
2022-10-19 16:28:46 +0200

    set placeholders in script

--- deploy/scripts/SetKeyVaultAccessPolicies.ps1
+++ deploy/scripts/SetKeyVaultAccessPolicies.ps1
@@ -2,15 +2,15 @@
 Login-AzAccount
 
 # Key vault name
-$VaultName = 'dev-we-lab-app-vault'
+$VaultName = 'dev-we-{your initials}-app-vault'
 # ObjectId's  array
-$ObjIDs = @("helena.coppieters@codit.eu")
+$ObjIDs = @("{your firstname}.{your lastname}@codit.eu")
 # keys access policies array
 #$KeysAP = @("all","get,list,delete","encrypt,decrypt")
 # secrets access policies array
 $SecretsAP = @("get,list","set,get,list,delete","backup,restore")
 Set-AzContext -SubscriptionId "533f6b10-3a70-4c5d-8962-8b51e839a13c"
-Set-AzKeyVaultAccessPolicy -VaultName 'dev-we-lab-app-vault' -UserPrincipalName 'Helena.Coppieters@codit.eu' -PermissionsToSecrets set,delete,get -PassThru
+Set-AzKeyVaultAccessPolicy -VaultName $VaultName -UserPrincipalName '{your firstname}.{your lastname}@codit.eu' -PermissionsToSecrets set,delete,get -PassThru
 Write-Host "*** Granting access policies: "
 For ($i = 0; $i -lt $ObjIDs.Length; $i++) {
     Write-Host "*** Create access policy"
